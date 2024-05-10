997071089c25a7e8dda96c023da77988b771b181
shashi kumar verma <shashik.verma@cloudactivelabs.in>
2024-05-10 13:03:34 +0530

    mui card done

---
+++ vite-project/src/component/progressBar.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "./progressBar.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
+
+const MultiStepProgressBar = ({ percentage,idx, onPageNumberClick }:{ percentage:number,idx:number, onPageNumberClick:unknown }) => {
+//   let stepPercentage:number = percentage;
+ 
+
+  return (
+    <ProgressBar percent={(100/(percentage - 1))*(idx) } style={{marginLeft:-20}}>
+     {
+        [1,1,1,1,1,1].map((e:any )=>(
+            <Step>
+        { ({ accomplished, index }:{accomplished:unknown , index:number})=> (
+          <div
+            className={`indexedStep accomplished`}
+          
+            style={{display: index !==idx && "none" , padding:5 , backgroundColor:"#9747FF" }}
+          >
+            <h1 style={{padding:5, backgroundColor:"#FFFFFF" ,borderRadius:20 }}></h1>
+          </div>
+        )}
+      </Step>
+        ))
+     }
+    </ProgressBar>
+  );
+};
+
+export default MultiStepProgressBar;
